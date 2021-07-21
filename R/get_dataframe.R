b79fee590f830ea2dc67cf5a49dab3c346817e17
Juan Ignacio Fulponi <juanignaciofulponi@MacBook-Air-de-Juan.local>
2021-07-21 13:50:22 -0300

    Version 0.1.1

--- R/get_dataframe.R
+++ R/get_dataframe.R
@@ -2,18 +2,20 @@
 #'
 #' Get a specified dataset in dataframe format directly from Alphacast API.
 #' @param dataset_id Dataset ID. It must be numeric.
-#' @param api_key API Key from Alphacast. You can get it from your Settings menu on the Alphacast Web.
+#' @param alphacast_api_key API Key from Alphacast. You can get it from your Settings menu on the Alphacast Web.
 #' @param long Logical. Choose between data format.
 #' @return A dataframe with dates, entity and data.
 #' @examples
-#' api_key = "ak_QDPD89fxwASlhWwdfeIO"
-#' resultad_fiscal <- get_dataframe(5542, api_key);
+#' alphacast_api_key = "ak_QDPD89fxwASlhWwdfeIO"
+#' resultad_fiscal <- get_dataframe(5542, alphacast_api_key);
 #' @export
+
+#' alphacast_api_key <- "ak_JGzP3xTSrj9e29e5bVCq"
 
 get_dataframe <- function(dataset_id, api_key, long = FALSE) {
   if (is.numeric(dataset_id)){
     r <- httr::GET(paste("https://charts.alphacast.io/api/datasets/", dataset_id,".csv", sep=""),
-                    httr::authenticate(user = api_key, password = ""))
+                    httr::authenticate(user = alphacast_api_key, password = ""))
   r <- content(r)
    if (isTRUE(long)) {
      r <- reshape2::melt(r, id.vars = c("Entity", "Year"))
--- R/get_repos_datasets.R
+++ R/get_repos_datasets.R
@@ -1,18 +1,18 @@
 #' Get datasets from the repo.
 #'
 #' Get all available datasets from Alphacast with your level of access in the specified repo.
-#' @param api_key API Key from Alphacast. You can get it from your Settings menu on the Alphacast Web.
+#' @param alphacast_api_key API Key from Alphacast. You can get it from your Settings menu on the Alphacast Web.
 #' @param repo_id Must be numeric. This is the id from the repo you want to get the datasets.
 #' @return A index with all datasets from the repo.
 #' @examples
-#' api_key = "ak_QDPD89fxwASlhWwdfeIO"
-#' arg_macro_basics <- get_repos_datasets(20, api_key)
+#' alphacast_api_key = "ak_QDPD89fxwASlhWwdfeIO"
+#' arg_macro_basics <- get_repos_datasets(20, alphacast_api_key)
 #' @export
 
 get_repos_datasets <- function(repo_id, api_key) {
   if (is.numeric(repo_id)){
     r <- httr::GET("https://api.alphacast.io/datasets", query = list(repo_id = repo_id),
-                   httr::authenticate(user = api_key, password = ""))
+                   httr::authenticate(user = alphacast_api_key, password = ""))
     r <- dplyr::bind_rows(content(r))
     r[which(r$repositoryId==repo_id), ]
   }
